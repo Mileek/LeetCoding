d7bd809f79854aaf934724587a7b5c175adf006d
Mileek <kamilex1998@gmail.com>
2024-05-04 23:07:50 +0200

    First commit,learning, just SumTwo as of yet

---
+++ LeetCodes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+// Definicja metody jako lokalnej funkcji w top-level statement
+int[] TwoSum(int[] nums, int target)
+{
+    Console.WriteLine("test");
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                return new int[] { i, j };
+            }
+        }
+    }
+    return new int[0]; // Przykładowy zwrot, zastąp właściwą logiką
+}
+
+var result = TwoSum(new int[] { 2, 11, 15, 7 }, 9);
+
+Console.WriteLine($"[{result[0]}, {result[1]}]");
---
+++ TwoSum.cs
@@ -0,0 +1,7 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Debug.WriteLine("test");
+    }
+}
