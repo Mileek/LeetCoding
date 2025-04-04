49b739a359da75a55b2263a3c7b407c5a5f881a9
Kamil Kaszuba <kamil.kaszuba@allcomp.pl>
2025-04-04 14:20:35 +0200

    Leet Prepared, and sum two

---
+++ LeetCodes/TwoSumClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+ */
+
+namespace LeetCodes
+{
+    internal class TwoSumClass
+    {
+        //  0  1   2   3
+        //{ 2, 11, 15, 7 }, 9 ma zwrócić 0 i 3
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[0];
+        }
+
+        public void Run()
+        {
+            var result = TwoSum(new int[] { 2, 11, 15, 7 }, 9);
+
+            Console.WriteLine($"[{result[0]}, {result[1]}]");
+        }
+    }
+}
