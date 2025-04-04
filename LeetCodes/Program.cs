6c329b4555548d88e8433d81bf8e8aefd08b13c3
Kamil Kaszuba <kamil.kaszuba@allcomp.pl>
2025-04-04 14:19:30 +0200

    Leet Prepared, and sum two

--- LeetCodes/Program.cs
+++ LeetCodes/Program.cs
@@ -1,25 +1,10 @@
-﻿using System.Diagnostics;
+﻿using LeetCodes;
 
-Console.WriteLine("Hello, World!");
-
-// Definicja metody jako lokalnej funkcji w top-level statement
-int[] TwoSum(int[] nums, int target)
+public class Program
 {
-    Console.WriteLine("test");
-
-    for (int i = 0; i < nums.Length; i++)
+    private static void Main(string[] args)
     {
-        for (int j = i + 1; j < nums.Length; j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                return new int[] { i, j };
-            }
-        }
+        var Test = new TwoSumClass();
+        Test.Run();
     }
-    return new int[0]; // Przykładowy zwrot, zastąp właściwą logiką
 }
-
-var result = TwoSum(new int[] { 2, 11, 15, 7 }, 9);
-
-Console.WriteLine($"[{result[0]}, {result[1]}]");
