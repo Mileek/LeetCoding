9a015bd54f5d630cb33d1f5089766475f4a8bc8b
Kamil Kaszuba <kamil.kaszuba@allcomp.pl>
2025-07-08 12:35:19 +0200

    Config files

--- LeetCodes/obj/Debug/net8.0/LeetCodes.AssemblyInfo.cs
+++ LeetCodes/obj/Debug/net8.0/LeetCodes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCodes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d8bdb4ac1b032123e39b8413f65bee08615e621")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1760f8d4581ebf5b5dd3d2dcad940ce87f04629")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCodes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCodes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
